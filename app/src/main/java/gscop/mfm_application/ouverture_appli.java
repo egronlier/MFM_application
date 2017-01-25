7b796d3e6dac0eca7630fc3e922a7319ae690a3d
egronlier <eloise.gronlier@hotmail.fr>
2017-01-25 14:44:16 +0100

    coucou test comit elo g

--- app/src/main/java/gscop/mfm_application/ouverture_appli.java
+++ app/src/main/java/gscop/mfm_application/ouverture_appli.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 public class ouverture_appli extends AppCompatActivity {
 
     @Override
+    /*
+     méthode appelée à l'initialisation
+    */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ouverture_appli);
