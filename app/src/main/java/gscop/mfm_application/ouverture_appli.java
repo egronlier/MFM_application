62c338a5d69ae02b491e1b255b842f44f352452d
egronlier <eloise.gronlier@hotmail.fr>
2017-01-25 10:25:16 +0100

    Initial commit

---
+++ app/src/main/java/gscop/mfm_application/ouverture_appli.java
@@ -0,0 +1,13 @@
+package gscop.mfm_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ouverture_appli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ouverture_appli);
+    }
+}
