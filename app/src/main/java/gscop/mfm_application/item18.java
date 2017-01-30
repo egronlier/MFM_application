6f77648293c9b9970d0d34422c6e968cfb69d0a9
eloiselgr <eloise.liogier@laposte.net>
2017-01-30 16:33:26 +0100

    Ajout du CD

---
+++ app/src/main/java/gscop/mfm_application/item18.java
@@ -0,0 +1,18 @@
+package gscop.mfm_application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Éloïse on 30/01/2017.
+ */
+
+public class item18 extends Activity {
+    /*
+     méthode appelée à l'initialisation
+    */
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consignes_item18);
+    }
+}
