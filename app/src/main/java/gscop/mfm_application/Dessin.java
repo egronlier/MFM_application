5a43af86738c621ceacc56096b0804ca4c2db72d
eloiselgr <eloise.liogier@laposte.net>
2017-02-01 09:40:30 +0100

    Ajout dessin sur CD (item18)

---
+++ app/src/main/java/gscop/mfm_application/Dessin.java
@@ -0,0 +1,117 @@
+package gscop.mfm_application;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Éloïse on 31/01/2017.
+ */
+
+//Cette classe permet de dessiner
+public class Dessin extends View {
+
+    public static final int MAX_FINGERS = 100;
+    private Path[] mFingerPaths = new Path[MAX_FINGERS];
+    private Paint mFingerPaint;
+    private ArrayList<Path> mCompletedPaths;
+    private RectF mPathBounds = new RectF();
+
+    public Dessin(Context context) {
+        super(context);
+    }
+
+    public Dessin(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Dessin(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mCompletedPaths = new ArrayList<Path>();
+        mFingerPaint = new Paint();
+        // initialise les caractéristiques du trait (forme, couleur...)
+        mFingerPaint.setAntiAlias(true);
+        mFingerPaint.setColor(Color.BLACK);
+        mFingerPaint.setStyle(Paint.Style.STROKE);
+        mFingerPaint.setStrokeWidth(20);
+        mFingerPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // On transforme le drawable du CD en bitmap
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.cd);
+        Bitmap cd = Bitmap.createScaledBitmap(image, 1080,1080, true);
+        // On ajoute ce bitmap au canvas pour pouvoir dessiner dessus : les deux nombres en paramètres servent à positionner le CD dans le canvas
+        canvas.drawBitmap(cd, 60, 60, null);
+        super.onDraw(canvas);
+        for (Path completedPath : mCompletedPaths) {
+            canvas.drawPath(completedPath, mFingerPaint);
+        }
+
+        for (Path fingerPath : mFingerPaths) {
+            if (fingerPath != null) {
+                canvas.drawPath(fingerPath, mFingerPaint);
+            }
+        }
+
+
+    }
+
+    //
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int pointerCount = event.getPointerCount();
+        int cappedPointerCount = pointerCount > MAX_FINGERS ? MAX_FINGERS : pointerCount;
+        int actionIndex = event.getActionIndex();
+        int action = event.getActionMasked();
+        int id = event.getPointerId(actionIndex);
+
+        if ((action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_POINTER_DOWN) && id < MAX_FINGERS) {
+            mFingerPaths[id] = new Path();
+            mFingerPaths[id].moveTo(event.getX(actionIndex), event.getY(actionIndex));
+        } else if ((action == MotionEvent.ACTION_POINTER_UP || action == MotionEvent.ACTION_UP) && id < MAX_FINGERS) {
+            mFingerPaths[id].setLastPoint(event.getX(actionIndex), event.getY(actionIndex));
+            mCompletedPaths.add(mFingerPaths[id]);
+            mFingerPaths[id].computeBounds(mPathBounds, true);
+            invalidate((int) mPathBounds.left, (int) mPathBounds.top,
+                    (int) mPathBounds.right, (int) mPathBounds.bottom);
+            mFingerPaths[id] = null;
+        }
+
+        for(int i = 0; i < cappedPointerCount; i++) {
+            if(mFingerPaths[i] != null) {
+                int index = event.findPointerIndex(i);
+                mFingerPaths[i].lineTo(event.getX(index), event.getY(index));
+                mFingerPaths[i].computeBounds(mPathBounds, true);
+                invalidate((int) mPathBounds.left, (int) mPathBounds.top,
+                        (int) mPathBounds.right, (int) mPathBounds.bottom);
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+
+}
--- app/src/main/java/gscop/mfm_application/do_item18.java
+++ app/src/main/java/gscop/mfm_application/do_item18.java
@@ -6,33 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class consignes_item18 extends Activity {
+/**
+ * Created by Éloïse on 01/02/2017.
+ */
+
+public class do_item18 extends Activity {
 
     Button boutonRetour;
-    Button boutonDemarrer;
+    Button boutonTerminer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.consignes_item18);
+        setContentView(R.layout.do_item18);
 
         boutonRetour = (Button) findViewById(R.id.boutonRetour);
         boutonRetour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(consignes_item18.this, choix_item.class);
+                Intent myIntent = new Intent(do_item18.this, consignes_item18.class);
                 startActivity(myIntent);
             }
         });
-
-        boutonDemarrer = (Button) findViewById(R.id.boutonDemarrer);
-        boutonDemarrer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent2 = new Intent(consignes_item18.this, do_item18.class);
-                startActivity(myIntent2);
-            }
-        });
     }
+
+
 }
