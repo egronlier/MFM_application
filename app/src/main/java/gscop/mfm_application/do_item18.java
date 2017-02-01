9f8b70662e2b260d30c1330c73b44a09b2a70d87
eloiselgr <eloise.liogier@laposte.net>
2017-02-01 09:49:31 +0100

    Ajout boîte de dialogue retour item18

--- app/src/main/java/gscop/mfm_application/do_item18.java
+++ app/src/main/java/gscop/mfm_application/do_item18.java
@@ -1,6 +1,9 @@
 package gscop.mfm_application;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +17,7 @@ public class do_item18 extends Activity {
 
     Button boutonRetour;
     Button boutonTerminer;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +29,33 @@ public class do_item18 extends Activity {
         boutonRetour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(do_item18.this, consignes_item18.class);
-                startActivity(myIntent);
+                // ouvrir une boite de dialogue permettant de valider les infos entrées
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                // set titre
+                alertDialogBuilder.setTitle("Confirmation");
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Êtes-vous certain de vouloir quitter l'exercice ? (le tracé sera perdu)")
+                        .setCancelable(false)
+                        .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // if this button is clicked, go to next activity
+                                dialog.cancel();
+                                // on lance l'activité de choix d'item
+                                Intent myIntent = new Intent(do_item18.this, consignes_item18.class);
+                                startActivity(myIntent);
+                            }
+                        })
+                        .setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // if this button is clicked, close the dialog box
+                                dialog.cancel();
+                            }
+                        });
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                // show it
+                alertDialog.show();
             }
         });
     }
