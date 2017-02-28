1f9a83eb25801b751bb33d343d6f03d8262b7ced
egronlier <eloise.gronlier@hotmail.fr>
2017-02-28 15:06:20 +0100

    version du 28/02 début aprem

--- app/src/main/java/gscop/mfm_application/comments_item22.java
+++ app/src/main/java/gscop/mfm_application/comments_item22.java
@@ -390,15 +390,15 @@ public class comments_item22 extends Activity implements MultiSelectionSpinner.O
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), R.string.viewPB, Toast.LENGTH_LONG).show();
                     }
-                    // on renvoie alors vers l'interface de choix d'item
-                    Intent myIntent = new Intent(comments_item22.this, choix_item.class);
-                    myIntent.putExtra("name", name);
-                    myIntent.putExtra("surname", surname);
-                    myIntent.putExtra("birthdate", birthdate);
-                    myIntent.putExtra("main", main);
-                    startActivity(myIntent);
-                    // on ferme l'activité en cours
-                    finish();
+//                    // on renvoie alors vers l'interface de choix d'item
+//                    Intent myIntent = new Intent(comments_item22.this, choix_item.class);
+//                    myIntent.putExtra("name", name);
+//                    myIntent.putExtra("surname", surname);
+//                    myIntent.putExtra("birthdate", birthdate);
+//                    myIntent.putExtra("main", main);
+//                    startActivity(myIntent);
+//                    // on ferme l'activité en cours
+//                    finish();
                 } else if (options[which].equals(getString(R.string.label_quit))) {
                     comments_item22.this.finish();
                     System.exit(0);
