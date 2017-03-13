9062aaa538873954773ef44b7e4db614e1c8d443
eloiselgr <eloise.liogier@laposte.net>
2017-03-13 16:28:04 +0100

    Commentaires choice_item

--- app/src/main/java/gscop/mfm_application/choice_item.java
+++ app/src/main/java/gscop/mfm_application/choice_item.java
@@ -12,9 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * The type Choice item.
- */
 public class choice_item extends Activity {
 
     private TextView textNomPrenomPatient;
@@ -34,7 +31,7 @@ public class choice_item extends Activity {
 
         textNomPrenomPatient = (TextView) findViewById(R.id.PatientName);
 
-        // on récupère les données de l'activité précédente pour afficher les données du patient
+        // On récupère les données de l'activité précédente pour afficher les données du patient
         Intent intent = getIntent();
         if (intent != null) {
             name = intent.getStringExtra("name");
@@ -43,6 +40,7 @@ public class choice_item extends Activity {
             textNomPrenomPatient.setText("Patient : " + name + " " + surname + " \nNé(e) le : " + birthdate);
         }
 
+        // Pour le bouton "Quitter"
         buttonExit = (Button) findViewById(R.id.buttonExit);
         buttonExit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +50,7 @@ public class choice_item extends Activity {
                         .setCancelable(true)
                         .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                // on quitte l'application courante
+                                // On quitte l'application courante
                                 choice_item.this.finish();
                                 System.exit(0);
                             }
@@ -71,41 +69,41 @@ public class choice_item extends Activity {
         buttonItem18.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // aléatoirement lance la version papier ou la version tablette
+                // Décision aléatoire de commencer par la version papier ou tablette
                 int mini = 0;
                 int max = 1;
-                // génère aléatoirement 0 ou 1
+                // Génère aléatoirement 0 ou 1
                 int varRandom = (int) (Math.random() * (max - mini + 1)) + mini;
                 if (varRandom == 0) {
-                    // on demande de réaliser l'item 18 version papier en premier
+                    // On demande de réaliser l'item 18 version papier en premier
                     AlertDialog.Builder builder = new AlertDialog.Builder(context);
                     builder.setMessage(R.string.paper18)
                             .setTitle("MFM Papier")
                             .setCancelable(false)
                             .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
-                                    // on lance les consignes de l'item 18
+                                    // On lance les consignes de l'item 18
                                     Intent myIntent = new Intent(choice_item.this, consignes_item18.class);
                                     myIntent.putExtra("name", name);
                                     myIntent.putExtra("surname", surname);
                                     myIntent.putExtra("birthdate", birthdate);
                                     myIntent.putExtra("varRandom", 0);
                                     startActivity(myIntent);
-                                    // on ferme l'activité en cours
+                                    // On ferme l'activité en cours
                                     finish();
                                 }
                             });
                     AlertDialog alert = builder.create();
                     alert.show();
                 } else {
-                    // on demande la version tablette en premier
+                    // On demande la version tablette en premier : on ouvre la nouvelle interface
                     Intent myIntent = new Intent(choice_item.this, consignes_item18.class);
                     myIntent.putExtra("name", name);
                     myIntent.putExtra("surname", surname);
                     myIntent.putExtra("birthdate", birthdate);
                     myIntent.putExtra("varRandom", 1);
                     startActivity(myIntent);
-                    // on ferme l'activité en cours
+                    // On ferme l'activité en cours
                     finish();
                 }
             }
@@ -115,41 +113,41 @@ public class choice_item extends Activity {
         buttonItem22.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // aléatoirement lance la version papier ou la version tablette
+                // Décision aléatoire de commencer par la version papier ou tablette
                 int mini = 0;
                 int max = 1;
-                // génère aléatoirement 0 ou 1
+                // Génère aléatoirement 0 ou 1
                 int varRandom = (int) (Math.random() * (max - mini + 1)) + mini;
                 if (varRandom == 0) {
-                    // on demande de réaliser l'item 18 version papier en premier
+                    // Oon demande de réaliser l'item 18 version papier en premier
                     AlertDialog.Builder builder = new AlertDialog.Builder(context);
                     builder.setMessage(R.string.paper22)
                             .setTitle("MFM Papier")
                             .setCancelable(false)
                             .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
-                                    // on lance les consignes de l'item 22
+                                    // On lance les consignes de l'item 22
                                     Intent myIntent = new Intent(choice_item.this, consignes_item22.class);
                                     myIntent.putExtra("name", name);
                                     myIntent.putExtra("surname", surname);
                                     myIntent.putExtra("birthdate", birthdate);
                                     myIntent.putExtra("varRandom", 0);
                                     startActivity(myIntent);
-                                    // on ferme l'activité en cours
+                                    // On ferme l'activité en cours
                                     finish();
                                 }
                             });
                     AlertDialog alert = builder.create();
                     alert.show();
                 } else {
-                    // on demande la version tablette en premier
+                    // On demande la version tablette en premier : on ouvre la nouvelle interface
                     Intent myIntent = new Intent(choice_item.this, consignes_item22.class);
                     myIntent.putExtra("name", name);
                     myIntent.putExtra("surname", surname);
                     myIntent.putExtra("birthdate", birthdate);
                     myIntent.putExtra("varRandom", 1);
                     startActivity(myIntent);
-                    // on ferme l'activité en cours
+                    // On ferme l'activité en cours
                     finish();
                 }
             }
@@ -168,11 +166,11 @@ public class choice_item extends Activity {
                     .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             back_answer = true;
-                            // on revient à l'écran d'accueil d'entrée des infos patient
+                            // On revient à l'écran d'accueil d'entrée des infos patient
                             Intent myIntent = new Intent(choice_item.this, ouverture_appli.class);
-                            // pas besoin des extra car pas d'infos à faire transiter
+                            // Pas besoin des extra car pas d'infos à transmettre
                             startActivity(myIntent);
-                            // on ferme l'activité en cours
+                            // On ferme l'activité en cours
                             finish();
                         }
                     })
